da645c6f285a341377f5f8489619b44b0ac135cb
Victor <victormwendwa804@gmail.com>
2024-01-10 11:06:20 -0800

    Token #1

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
 # Check if the argument contains repetition of "t" followed by zero or more "n"
-if ARGV[0] =~ /tn*/
-    puts ARGV[0]
+if match_data = ARGV[0].match(/hbt*n/)
+    puts match_data[0]
 else
   puts "$"
 end
